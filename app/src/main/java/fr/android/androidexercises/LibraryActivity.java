5c9ba4c9de33fbc8392e0f757db12183efd32b81
Marie Comon <mcomon15@etudiant.mines-nantes.fr>
2017-12-19 09:49:57 +0100

    Etape 10

--- app/src/main/java/fr/android/androidexercises/LibraryActivity.java
+++ app/src/main/java/fr/android/androidexercises/LibraryActivity.java
@@ -2,10 +2,20 @@ package fr.android.androidexercises;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Random;
+
 
 public class LibraryActivity extends AppCompatActivity {
 
@@ -15,11 +25,21 @@ public class LibraryActivity extends AppCompatActivity {
         setContentView(R.layout.activity_library);
         Toolbar toolbar = findViewById(R.id.toolbar);
 
+
         TextView messageTextView = findViewById(R.id.messageTextView);
         // TODO call setText() on messageTextView
 
         setSupportActionBar(toolbar);
     }
+
+        //List<Book> books = getBooks();
+
+        //ListView lv = findViewById(R.id.bookListView);
+        //lv.setAdapter(new BookAdapter(LibraryActivity.this, books));
+
+        RecyclerView rv = findViewById(R.id.bookListView);
+        //rv.setLayoutManager(new LinearLayoutManager(this));
+        //rv.setAdapter(new BookRecyclerAdapter(this, books));
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
---
+++ app/src/main/java/fr/android/androidexercises/LibraryActivity2.kt
@@ -0,0 +1,45 @@
+package fr.android.androidexercises
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v7.widget.Toolbar
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.TextView
+
+/**
+ * Created by Marie Comon on 19/12/2017.
+ */
+class LibraryActivity2 : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_library)
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+
+
+        val messageTextView = findViewById<TextView>(R.id.messageTextView)
+        // TODO call setText() on messageTextView
+
+        setSupportActionBar(toolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_library, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        val id = item.itemId
+
+
+        return if (id == R.id.action_settings) {
+            true
+        } else super.onOptionsItemSelected(item)
+
+    }
+}
